f53921857e6dc59603d87b341efed436c0158a7b
Oskar Nysæther <oskarsn@stud.ntnu.no>
2026-09-21 09:47:45 +0200

    added slave select to read and write

--- spi.c
+++ spi.c
@@ -4,7 +4,7 @@
 void slave_select(Slave slave)
 {
     // Deselect all slaves
-    PORTB |= (1 << PB2) | (1 << PB1) | (1 << PB0);
+    PORTB |= (1 << PB4) | (1 << PB1) | (1 << PB0);
 
 
     switch (slave) {
@@ -23,14 +23,16 @@ void slave_select(Slave slave)
     }
 }
 
-void write_byte(uint8_t data)
+void write_byte(uint8_t data, Slave slave)
 {
+    slave_select(slave); // Select the appropriate slave
     SPDR = data; // Load data into the SPI Data Register
     while (!(SPSR & (1 << SPIF))); // Wait for transmission to complete
 }
 
-uint8_t read_byte(void)
+uint8_t read_byte(Slave slave)
 {
+    slave_select(slave); // Select the appropriate slave
     SPDR = 0xFF; // Send dummy data to initiate SPI read
     while (!(SPSR & (1 << SPIF))); // Wait for reception to complete
     return SPDR; // Return the received data
