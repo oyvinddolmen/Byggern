e7efcbd8a2d2409f31c879006932724f6a25e94a
Oskar Nysæther <oskarsn@stud.ntnu.no>
2026-09-21 09:51:06 +0200

    transfer instead of read/write

--- spi.c
+++ spi.c
@@ -68,17 +68,11 @@ void slave_select(Slave slave)
     }
 }
 
-void write_byte(uint8_t data, Slave slave)
+uint8_t transfer_byte(uint8_t data, Slave slave)
 {
     slave_select(slave); // Select the appropriate slave
     SPDR = data; // Load data into the SPI Data Register
     while (!(SPSR & (1 << SPIF))); // Wait for transmission to complete
-}
 
-uint8_t read_byte(Slave slave)
-{
-    slave_select(slave); // Select the appropriate slave
-    SPDR = 0xFF; // Send dummy data to initiate SPI read
-    while (!(SPSR & (1 << SPIF))); // Wait for reception to complete
     return SPDR; // Return the received data
 }
--- spi.h
+++ spi.h
@@ -8,6 +8,5 @@ typedef enum {
 } Slave;
 
 void slave_select(Slave slave);
-void write_byte(uint8_t data, Slave slave);
-uint8_t read_byte(Slave slave);
+uint8_t transfer_byte(uint8_t data, Slave slave);
 void spi_init(void);
