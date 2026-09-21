97f2ebd4abd1c7b121dccac87ce29509b6983f0d
Oskar Nysæther <oskarsn@stud.ntnu.no>
2026-09-21 09:17:33 +0200

    start of io programming

---
+++ oled.h
@@ -0,0 +1,11 @@
+#include <avr/io.h>
+#include <stdio.h>
+
+oled_init();
+oled_reset();
+oled_home();
+oled_goto_line(line);
+oled_goto_column(column);
+oled_clear_line(line);
+oled_pos(row,column);
+oled_print(char*);
