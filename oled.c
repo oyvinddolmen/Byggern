03485ab126f36f54cc83c0b8c4973b2853fc000e
Øyvind <oyvind.dolmen@gmail.com>
2026-09-21 10:22:42 +0200

    oled init

--- oled.c
+++ oled.c
@@ -4,7 +4,29 @@
 
 void oled_init(void)
 {
-    // OLED initialization
+    // fra 
+    oled_write_command(0xae); // display off
+    oled_write_command(0xa1); //segment remap
+    oled_write_command(0xda); //common pads hardware: alternative
+    oled_write_command(0x12);
+    oled_write_command(0xc8); //common output scan direction:com63~com0
+    oled_write_command(0xa8); //multiplex ration mode:63
+    oled_write_command(0x3f);
+    oled_write_command(0xd5); //display divide ratio/osc. freq. mode
+    oled_write_command(0x80);
+    oled_write_command(0x81); //contrast control
+    oled_write_command(0x50);
+    oled_write_command(0xd9); //set pre-charge period
+    oled_write_command(0x21);
+    oled_write_command(0x20); //Set Memory Addressing Mode
+    oled_write_command(0x02);
+    oled_write_command(0xdb); //VCOM deselect level mode
+    oled_write_command(0x30);
+    oled_write_command(0xad); //master configuration
+    oled_write_command(0x00);
+    oled_write_command(0xa4); //out follows RAM content
+    oled_write_command(0xa6); //set normal display
+    oled_write_command(0xaf); // display on
 }
 
 void oled_reset(void)
